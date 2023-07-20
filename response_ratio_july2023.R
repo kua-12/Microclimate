4bfc6ffa8d166c449328d0cab2820e828c9b6167
Naomi Schwartz Office Desktop <naomibschwartz@gmail.com>
2023-07-20 14:56:19 -0700

    Added some analyses of the SWP data, and included the measurements w units "mm" in the response ratio calculations

--- response_ratio_july2023.R
+++ response_ratio_july2023.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 #this is a simple script to calculate the response ratio, add it to the dataset, and fit a simple mixed effects model to calculate the average RR accounting for the multiple observations from studies
 
 library(tidyverse)
@@ -9,7 +11,7 @@ microclim <- microclim %>% mutate(datapoint.num = 1:nrow(microclim))
 
 microclim_tidy <- microclim %>%
   select(unique_id, datapoint.num, moisture_type_consistent, moisture_units, moisture_open, moisture_forest) %>%
-  filter(moisture_units %in% c('cm^3/cm^3', '%', 'm^3/m^3', 'm^3/ m^3', "mm/mm", "g kg -1", 'mm/cm')) %>%
+  filter(moisture_units %in% c('cm^3/cm^3', '%', 'm^3/m^3', 'm^3/ m^3', "mm/mm", "g kg -1", 'mm/cm', 'mm')) %>%
   mutate(rr = log(moisture_forest/moisture_open))
 
 ##choose the measurements with "proportion units"
@@ -38,3 +40,17 @@ library(lme4)
 
 simp_mod <- with(microclim_tidy, lmer(rr ~ 1|unique_id))
 summary(simp_mod)
+
+
+##for the ones where we can't calculated a RR (swp), compare the forest and open sites
+
+swp <- microclim %>% filter(moisture_type_consistent %in% c('SMP', 'SWP'))
+unique(swp$moisture_units)
+
+#need to convert MPa to kPa
+swp <- swp %>% mutate(moisture_forest_corrected = ifelse(moisture_units == 'MPa', moisture_forest*1000, moisture_forest), moisture_open_corrected = ifelse(moisture_units == 'MPa', moisture_open*1000, moisture_open)) %>% mutate(moisture_diff = moisture_forest_corrected - moisture_open_corrected)
+
+hist(swp$moisture_diff)  #most values are negative, indicating that forest tends to be drier.
+
+##lingering question is how to do a response ratio for these values. Maybe, take the absolute value and then do the LRRd (used in medicine wehre negative nubmers indicate improved performance???)
+
